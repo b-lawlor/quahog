7f0942d82884c718f75514957bb5cb94af587804
Brendan Lawlor <blawlor@clamsnet.org>
2024-07-24 07:37:36 -0400

    clean up theme files

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -10,11 +10,7 @@ import Mermaid from './components/Mermaid.vue'
 import TestComponent from './components/TestComponent.vue'
 import Bug from './components/Bug.vue'
 import BugList from './components/BugList.vue'
-import Xkcd from './components/Xkcd.vue'
 
-// import StartLayout from './layouts/StartLayout.vue'
-// import Gallery from './components/Gallery.vue'
-// import Figure from './components/Figure.vue'
 import './style.css'
 
 export default {
@@ -22,10 +18,8 @@ export default {
     Layout: CustomLayout,
     enhanceApp({ app, router, siteData }) {
         app.component( 'Mermaid', Mermaid )
-        app.component( 'TestComponent', TestComponent)
         app.component( 'Bug', Bug)
         app.component( 'BugList', BugList)
-        app.component( 'Xkcd', Xkcd)
         // app.component( 'start', StartLayout )
         // app.component( 'Gallery', Gallery )
         // app.component( 'Figure', Figure )
